5c000f2857d11a7a6efaf5b7fd4c24623c4ac099
Brian Frisbie <bfrizb@github.com>
2016-05-06 23:00:11 -0700

    Updated pre-commit hook to fail if version number is not updated to what's expected

--- _precommit_hook.sh
+++ _precommit_hook.sh
@@ -5,4 +5,11 @@ num_revs=$(git rev-list --count HEAD)
 revs_at_last_minor_version=14
 let build_number=${num_revs}-${revs_at_last_minor_version}
 
-sed -i "" "s/__version__ =.*/__version__ = '"${current_minor_version}.${build_number}"'/" setup.py
+if ! grep -q "__version__ = '"${current_minor_version}.${build_number} setup.py; then
+    sed -i "" "s/__version__ =.*/__version__ = '"${current_minor_version}.${build_number}"'/" setup.py
+    echo
+    echo '*ABORTING* "git commit" and updating __version__ in setup.py to what is expected'
+    git add setup.py
+    git status
+    exit -1
+fi
